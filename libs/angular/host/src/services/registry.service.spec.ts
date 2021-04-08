2fe407d1713f73325c97018cde3c8acf915e0b21
IKatsuba <katsuba.igor@gmail.com>
2021-04-08 17:28:23 +0300

    ci: tests

--- libs/angular/host/src/services/registry.service.spec.ts
+++ libs/angular/host/src/services/registry.service.spec.ts
@@ -1,12 +1,15 @@
 import {TestBed} from '@angular/core/testing';
 
 import {RegistryService} from './registry.service';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
 
 describe('RegistryService', () => {
   let service: RegistryService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(RegistryService);
   });
 
