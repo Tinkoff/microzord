db7a56cc3f3c4822a63f0c149ffd86c98c39183c
Roman Sedov <darragon-nn@yandex.ru>
2021-04-19 16:06:45 +0300

    chore: improve errors expirience (#13)

--- libs/core/src/lib/helpers/load-app-constructor.ts
+++ libs/core/src/lib/helpers/load-app-constructor.ts
@@ -1,12 +1,9 @@
 import {defer, isObservable, Observable, of} from 'rxjs';
-import {switchMap, tap} from 'rxjs/operators';
+import {catchError, switchMap, tap} from 'rxjs/operators';
 import {ApplicationConstructor} from '../models/application';
-import {RegistrationOptions} from '../models/registration-options';
 import {appOptionsRegistry, loadedAppRegistry} from '../registry';
 import {getAppConstructor} from './get-app-constructor';
 
-// todo: подумать над ошибками, если в опциях нет функции загрузки
-// или приложение вообще не зарегистрированно
 export function loadAppConstructor<T extends Record<string, any> = Record<string, any>>(
   appName: string,
 ): Observable<ApplicationConstructor<T>> {
@@ -18,28 +15,20 @@ export function loadAppConstructor<T extends Record<string, any> = Record<string
             const appOptions = appOptionsRegistry.get(appName);
 
             if (!appOptions) {
-              throw new Error(
-                `Roofer appliction "${appName}" has not been registered. Check the spelling or register an app.`,
-              );
+              throw `Roofer appliction "${appName}" has not been registered. Check the spelling or register an app.`;
             }
 
             if (!appOptions.loadApp) {
-              throw new Error(
-                `Roofer appliction "${appName}" is registered but it has no "loadApp" function. Please, provide it`,
-              );
+              throw `Roofer appliction "${appName}" is registered but it has no "loadApp" function. Please, provide it`;
             }
 
             const result = appOptions.loadApp(appOptions.props);
 
-            if (
-              result &&
+            return result &&
               (isObservable(result) ||
                 ('then' in result && typeof result['then'] === 'function'))
-            ) {
-              return defer(() => result);
-            }
-
-            return of(result as ApplicationConstructor<T>);
+              ? defer(() => result)
+              : of(result as ApplicationConstructor<T>);
           }),
     ),
     tap(applicationConstructor => loadedAppRegistry.set(appName, applicationConstructor)),
