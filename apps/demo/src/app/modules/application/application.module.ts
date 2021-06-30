144644dd78556e62f1ee3904ad9eca77ca858c67
MarsiBarsi <darragon-nn@yandex.ru>
2021-06-30 18:21:29 +0300

    docs: add application demo page

--- apps/demo/src/app/modules/application/application.module.ts
+++ apps/demo/src/app/modules/application/application.module.ts
@@ -1,13 +1,14 @@
 import {NgModule} from '@angular/core';
 import {RouterModule} from '@angular/router';
 import {generateRoutes, TuiAddonDocModule} from '@taiga-ui/addon-doc';
-import {TuiLinkModule} from '@taiga-ui/core';
+import {TuiLinkModule, TuiNotificationModule} from '@taiga-ui/core';
 import {ApplicationComponent} from './application.component';
 
 @NgModule({
   imports: [
     TuiLinkModule,
     TuiAddonDocModule,
+    TuiNotificationModule,
     RouterModule.forChild(generateRoutes(ApplicationComponent)),
   ],
   declarations: [ApplicationComponent],
