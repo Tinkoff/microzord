b185f6f58e6c75d9107fd6cbe738f4cb63ae55fc
Igor Katsuba <i.katsuba@tinkoff.ru>
2021-10-25 14:39:29 +0300

    chore(vue): disable the build task

--- libs/vue/jest.config.js
+++ libs/vue/jest.config.js
@@ -2,18 +2,21 @@ module.exports = {
   displayName: 'vue',
   preset: '../../jest.preset.js',
   transform: {
-    '^.+\\.vue$': 'vue-jest',
+    '^.+\\.vue$': 'vue3-jest',
     '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
     '^.+\\.tsx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'vue', 'js', 'json'],
   coverageDirectory: '../../coverage/libs/vue',
-
+  snapshotSerializers: ['jest-serializer-vue'],
   globals: {
-    'ts-jest': {tsconfig: '<rootDir>/tsconfig.spec.json'},
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      babelConfig: '<rootDir>/babel.config.js',
+    },
     'vue-jest': {
-      tsConfig: '<rootDir>/tsconfig.spec.json',
+      tsConfig: 'libs/vue/tsconfig.spec.json',
+      babelConfig: 'libs/vue/babel.config.js',
     },
   },
-  snapshotSerializers: ['jest-serializer-vue'],
 };
